10271dcec62386c906c15914e8edebd34cda2049
Eduardo Martinez Cobos <eemece2@gmail.com>
2016-01-04 19:53:17 +0100

    Lambert material and group of objects

--- js/three-container.directive.js
+++ js/three-container.directive.js
@@ -26,11 +26,12 @@ angular.module('three', [])
         var scene = new THREE.Scene();
         // Camera
         var camera = new THREE.PerspectiveCamera(75, containerWith / containerHeight, 0.1, 1000);
+        camera.position.z = 5;
         // Lights
-        var ambient = new THREE.AmbientLight( 0x101010 );
+        var ambient = new THREE.AmbientLight( 0x505050 );
         scene.add( ambient );
         var light = new THREE.DirectionalLight( 0xefefff, 1.5 );
-        light.position.set( -10, -1, -1 ).normalize();
+        light.position.set( 1, 1, 1 ).normalize();
         scene.add( light );
 
         // Renderer
@@ -39,23 +40,40 @@ angular.module('three', [])
         // DOM
         element.append(renderer.domElement);
 
-        // Cube
-        var geometry = new THREE.BoxGeometry(1, 1, 1);
-        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        var cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-
-        camera.position.z = 5;
+        // Objects
+        makeObjects(scene);
 
         function render() {
             requestAnimationFrame(render);
 
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
+            //group.rotation.x += 0.01;
+            //group.rotation.y += 0.01;
+            camera.position.y += 0.01;
+            camera.lookAt(0, 0, 0);
 
             renderer.render(scene, camera);
         }
         render();
+
+        function makeObjects(scene) {
+            // Cube
+            var geometry = new THREE.BoxGeometry(1, 1, 1);
+            var material = new THREE.MeshLambertMaterial( { color: 0xff8866, overdraw: 0.5 } );
+            var cube = new THREE.Mesh(geometry, material);
+            //scene.add(cube);
+
+            // Cilinder
+            var geometryC = new THREE.CylinderGeometry(0.4, 0.4, 2, 32, 32);
+            var materialC = new THREE.MeshLambertMaterial( { color: 0x88ff66, overdraw: 0.5 } );
+            var cylinder = new THREE.Mesh(geometryC, materialC);
+            //scene.add(cilinder);
+
+            var group = new THREE.Object3D();
+            group.add(cube);
+            group.add(cylinder);
+
+            scene.add(group);
+        }
 
 
         scope.onWindowResize = function() {
